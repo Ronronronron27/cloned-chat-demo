e6067467270ea9d2b65f3408d6abae18818e5ccf
Ronronronron27 <guoronron@outlook.com>
2025-06-21 18:52:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from utils import generate_script
 st.title("🎬 视频脚本生成器")
 
 with st.sidebar:
-    openai_api_key = st.text_input("请输入你的API秘钥：", type="password")
+    openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
     st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-keys)")
 
-subject = st.text_input("请输入视频的主题")
-video_length = st.number_input("请输入视频的大致时长（单位：分钟）",min_value=0.1,step=0.1)
-creativity = st .slider("请输入视频脚本的创造力（数字小说明更严谨，数字大说明更多样）", min_value=0.0,
-                        max_value=1.0, value=0.2, step=0.1)
+subject = st.text_input("💡 请输入视频的主题")
+video_length = st.number_input("⏱️ 请输入视频的大致时长（单位：分钟）", min_value=0.1, step=0.1)
+creativity = st.slider("✨ 请输入视频脚本的创造力（数字小说明更严谨，数字大说明更多样）", min_value=0.0,
+                       max_value=1.0, value=0.2, step=0.1)
 submit = st.button("生成脚本")
 
 if submit and not openai_api_key:
@@ -20,15 +20,15 @@ if submit and not subject:
     st.info("请输入视频的主题")
     st.stop()
 if submit and not video_length >= 0.1:
-    st.info("视频的时长需要大于0.1分钟")
+    st.info("视频长度需要大于或等于0.1")
     st.stop()
 if submit:
     with st.spinner("AI正在思考中，请稍等..."):
-        search_result, title, script = generate_script(subject, video_length, creativity,openai_api_key)
-    st.success("视频脚本已经生成！")
-    st.subheader("标题：")
+        search_result, title, script = generate_script(subject, video_length, creativity, openai_api_key)
+    st.success("视频脚本已生成！")
+    st.subheader("🔥 标题：")
     st.write(title)
-    st.subheader("视频脚本：")
+    st.subheader("📝 视频脚本：")
     st.write(script)
-    with st.expander("维基百科搜索结果:"):
+    with st.expander("维基百科搜索结果 👀"):
         st.info(search_result)
--- utils.py
+++ utils.py
@@ -2,27 +2,30 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import WikipediaAPIWrapper
 
+# import os
+
 def generate_script(subject, video_length, creativity, api_key):
-    title_temple = ChatPromptTemplate.from_messages(
+    title_template = ChatPromptTemplate.from_messages(
         [
-            ("huaman", "请为'{subject}'这个主题的视频想一个吸引人的标题")
+            ("human", "请为'{subject}'这个主题的视频想一个吸引人的标题")
         ]
     )
-    script_temple = ChatPromptTemplate.from_messages(
+    script_template = ChatPromptTemplate.from_messages(
         [
-            ("huaman",
-             """你是一位短视频的博主。根据以下标题和相关信息，为短视频频道写一个视频脚本。
+            ("human",
+             """你是一位短视频频道的博主。根据以下标题和相关信息，为短视频频道写一个视频脚本。
              视频标题：{title}，视频时长：{duration}分钟，生成的脚本的长度尽量遵循视频时长的要求。
-             要求开头抓住眼球，中间提供干货内容，结尾有惊喜。脚本格式也请按照【开头、中间、结尾】分隔。
-             整体内容可以结合一下维基百科搜索出的信息，但仅供参考，只结合相关的即可，对不相关的进行忽略：
-             '''{wikipedia_search}'''""")
+             要求开头抓住限球，中间提供干货内容，结尾有惊喜，脚本格式也请按照【开头、中间，结尾】分隔。
+             整体内容的表达方式要尽量轻松有趣，吸引年轻人。
+             脚本内容可以结合以下维基百科搜索出的信息，但仅作为参考，只结合相关的即可，对不相关的进行忽略：
+             ```{wikipedia_search}```""")
         ]
     )
 
-    model = ChatOpenAI(Open_api_key = api_key, temperature = creativity)
+    model = ChatOpenAI(openai_api_key=api_key, temperature=creativity)
 
-    title_chain = title_temple | model
-    script_chain = script_temple | model
+    title_chain = title_template | model
+    script_chain = script_template | model
 
     title = title_chain.invoke({"subject": subject}).content
 
@@ -31,5 +34,7 @@ def generate_script(subject, video_length, creativity, api_key):
 
     script = script_chain.invoke({"title": title, "duration": video_length,
                                   "wikipedia_search": search_result}).content
-    return title, script, search_result
 
+    return search_result, title, script
+
+# print(generate_script("sora模型", 1, 0.7, os.getenv("OPENAI_API_KEY")))
